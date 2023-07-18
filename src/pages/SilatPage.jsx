51918686b2963b48017cf73af9d339734ec0fea5
AldoWijaya27 <aldo.wijaya2012@students.unila.ac.id>
2023-07-18 08:41:55 +0700

    fix (silat page): fixed bug on type writer

--- src/pages/SilatPage.jsx
+++ src/pages/SilatPage.jsx
@@ -1,48 +1,82 @@
-import React from "react";
-import silatAthlete from "../assets/athlete/silat-athlete.png";
-// import speechBubble from "../assets/pixel-speech-bubble.png";
-// import { Typewriter } from "react-simple-typewriter";
-import bangunan from "../assets/img-background/bangunan-3.png";
-import batu from "../assets/img-background/batu.png";
-import obor1 from "../assets/img-background/obor.png";
-import obor2 from "../assets/img-background/obor.png";
-import obor3 from "../assets/img-background/obor.png";
-import obor4 from "../assets/img-background/obor.png";
+import React, { useState, useEffect } from 'react';
+import silatAthlete from '../assets/athlete/silat-athlete.png';
+import bangunan from '../assets/img-background/bangunan-3.png';
+import batu from '../assets/img-background/batu.png';
+import obor1 from '../assets/img-background/obor.png';
+import obor2 from '../assets/img-background/obor.png';
+import obor3 from '../assets/img-background/obor.png';
+import obor4 from '../assets/img-background/obor.png';
+import Typewriter from '../components/TyperWriter';
 
-function SilatPage(){
-  
-  return(
-    <div className="silat">
-      <div className="silat-background">
-        <img src={bangunan} alt="bangunan gelap" id="silat-bangunan"/>
-        <img src={batu} alt="batu" id="silat-batu"/>
-        <div className="silat-obor-container">
-          <img src={obor1} alt="obor" className="obor"/>
-          <img src={obor2} alt="obor" className="obor"/>
-          <img src={obor3} alt="obor" className="obor"/>
-          <img src={obor4} alt="obor" className="obor"/>
+function SilatPage() {
+  const [typewriter1Visible, setTypewriter1Visible] = useState(false);
+  const [typewriter2Visible, setTypewriter2Visible] = useState(false);
+  const [typewriter3Visible, setTypewriter3Visible] = useState(false);
+  const [typewriter4Visible, setTypewriter4Visible] = useState(false);
+
+  useEffect(() => {
+    const delay = 200; // Waktu penundaan antara munculnya setiap Typewriter (ms)
+
+    const typewriter1Timer = setTimeout(() => {
+      setTypewriter1Visible(true);
+    }, delay);
+
+    const typewriter2Timer = setTimeout(() => {
+      setTypewriter2Visible(true);
+    }, delay * 4);
+
+    const typewriter3Timer = setTimeout(() => {
+      setTypewriter3Visible(true);
+    }, delay * 12);
+
+    const typewriter4Timer = setTimeout(() => {
+      setTypewriter4Visible(true);
+    }, delay * 16);
+
+    return () => {
+      clearTimeout(typewriter1Timer);
+      clearTimeout(typewriter2Timer);
+      clearTimeout(typewriter3Timer);
+      clearTimeout(typewriter4Timer);
+    };
+  }, []);
+
+  return (
+    <div className='silat'>
+      <div className='silat-background'>
+        <img src={bangunan} alt='bangunan gelap' id='silat-bangunan' />
+        <img src={batu} alt='batu' id='silat-batu' />
+        <div className='silat-obor-container'>
+          <img src={obor1} alt='obor' className='obor' />
+          <img src={obor2} alt='obor' className='obor' />
+          <img src={obor3} alt='obor' className='obor' />
+          <img src={obor4} alt='obor' className='obor' />
         </div>
       </div>
       <h3>Sejarah Silat di Indonesia</h3>
-      <div className="silat-top">
-        {/* <Typewriter 
-          words={["HALO! MARI KITA MULAI MEMPELAJARI SEJARAH DARI SILAT DI INDONESIA. TEKAN START UNTUK MEMULAI."]} 
-          loop={false} 
-          typeSpeed={80}
-          delaySpeed={4000}
-          deleteSpeed={0.2} 
-        /> */}
-        <p>HALO!<br/>
-        MARI KITA MULAI MEMPELAJARI SEJARAH<br/>
-        DARI SILAT DI INDONESIA.<br/>
-        TEKAN START UNTUK MEMULAI.</p>
-        {/* <img src={speechBubble} alt="speech bubble"></img> */}
-        <div className="silat-top_text-button">
-          <img src={silatAthlete} alt="athlete silat"></img>
-          <a href="/#">START</a>
+      <div className='silat-top'>
+        <div className='type-writer-wrapper'>
+          <>
+            {typewriter1Visible && <Typewriter text='Halo!' delay={100} />}
+            {typewriter2Visible && (
+              <Typewriter
+                text='MARI KITA MULAI MEMPELAJARI SEJARAH'
+                delay={100}
+              />
+            )}
+            {typewriter3Visible && (
+              <Typewriter text='SEJARAH SILAT DI INDONESIA' delay={100} />
+            )}
+            {typewriter4Visible && (
+              <Typewriter text='TEKAN START UNTUK MEMULAI.' delay={100} />
+            )}
+          </>
+        </div>
+        <div className='silat-top_text-button'>
+          <img src={silatAthlete} alt='athlete silat'></img>
+          <a href='/#'>START</a>
         </div>
       </div>
-
     </div>
   );
 }
