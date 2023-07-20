02d0ce6d86e8a9f2e89b8bf6205b9eaeac6a22e4
AldoWijaya27 <aldo.wijaya2012@students.unila.ac.id>
2023-07-20 12:15:05 +0700

    change (catur history desc): add new descriptions to catur history

--- src/pages/history/CaturHistory.jsx
+++ src/pages/history/CaturHistory.jsx
@@ -2,46 +2,47 @@ import React from 'react';
 import '../../styles/history.css';
 import tali from '../../assets/img-sejarah/tali.png';
 import catur from '../../assets/img-sejarah/catur.png';
-import obor from '../../assets/img-sejarah/obor.gif';
 import bgCatur from '../../assets/img-sejarah/bg-catur.png';
 import FooterHistory from '../../components/FooterHistory';
 import ArrowUp from '../../components/ArrowUp';
+import BackgroundAwan from '../../components/BackgroundAwan';
 
 const SilatHistory = () => {
   const dataExperience = [
     {
-      date: 'Prasejarah',
-      desc: 'Nenek moyang bangsa Indonesia mengembangkan cara pembelaan diri yang menirukan gerakan binatang di alam sekitar. Keterampilan bela diri suku-suku asli Indonesia berkembang melalui berburu dan berperang dengan menggunakan parang, perisai, dan tombak.',
+      date: 'Pengenalan Catur ke Indonesia:',
+      desc: 'Catur diperkenalkan ke Indonesia melalui kontak dengan bangsa India pada zaman perdagangan maritim kuno. Permainan catur membawa konsep intelektual dan strategi yang menarik minat kalangan intelektual di Indonesia.',
     },
     {
-      date: '1100 - 2000 M',
-      desc: 'Silat diturunkan secara lisan dan diwariskan dari guru ke murid. Beragam legenda mengenai asal mula silat, seperti legenda Minangkabau yang mengisahkan Datuk Suri Diraja menciptakan silek di Pariangan, Tanah Datar pada abad ke-11.Tokoh persilatan (pendekar) menjadi banggaan di setiap daerah, misalnya Prabu Siliwangi, Hang Tuah, Gajah Mada, dan Si Pitung.',
+      date: 'Perkembangan di Kalangan Intelektual',
+      desc: 'Catur menjadi populer di kalangan intelektual Indonesia pada abad ke-19 dan awal abad ke-20. Beberapa komunitas catur mulai terbentuk di berbagai daerah di Indonesia.',
     },
     {
-      date: '1400 - 1900 M',
-      desc: 'Penyebaran Islam di Nusantara pada abad ke-14 mempengaruhi perkembangan silat. Pencak silat diajarkan bersama-sama dengan pelajaran agama di surau atau pesantren. Silat menjadi bagian dari latihan spiritual dan budaya dalam beberapa suku bangsa di Indonesia.',
+      date: 'Keikutsertaan Pertama dalam Olimpiade Catur',
+      desc: 'Pada tahun 1950, Indonesia mengikuti Olimpiade Catur untuk pertama kalinya di Dubrovnik, Yugoslavia (sekarang Kroasia).Indonesia berpartisipasi sebagai negara non-anggota FIDE (Fédération Internationale des Échecs) pada saat itu. Islam di Nusantara pada abad ke-14 mempengaruhi perkembangan silat. Pencak silat diajarkan bersama-sama dengan pelajaran agama di surau atau pesantren. Silat menjadi bagian dari latihan spiritual dan budaya dalam beberapa suku bangsa di Indonesia.',
     },
     {
-      date: '1700 - 1900 M',
-      desc: 'Silat digunakan sebagai ilmu beladiri dalam perjuangan melawan penjajah Belanda. Para pendekar seperti Panembahan Senopati, Sultan Agung, Pangeran Diponegoro, Teungku Chik di Tiro, Teuku Umar, Tuanku Imam Bonjol, Sabai Nan Aluih, Cut Nyak Dhien, dan Cut Nyak Meutia mengangkat senjata dalam perjuangan melawan penjajah.',
+      date: 'Pembentukan Persatuan Catur Seluruh Indonesia (PECASI) (1953):',
+      desc: 'Pada tanggal 20 Desember 1953, Persatuan Catur Seluruh Indonesia (PECASI) resmi didirikan. PECASI bertujuan untuk mengembangkan dan mengatur olahraga catur di Indonesia.',
     },
     {
-      date: '1948 - 1980 M',
-      desc: 'Pada tanggal 18 Mei 1948, terbentuklah Ikatan Pencak Silat Indonesia (IPSI) sebagai organisasi pencak silat nasional pertama di Indonesia. Pada 11 Maret 1980, Persatuan Pencak Silat Antarbangsa (Persilat) didirikan atas prakarsa Eddie M. Nalapraya, dan Indonesia bersama Malaysia, Singapura, dan Brunei termasuk pendiri Persilat.',
+      date: 'Kemenangan Emas Pertama dalam Olimpiade Catur (1962)',
+      desc: 'Pada Olimpiade Catur tahun 1962 di Varna, Bulgaria, tim catur Indonesia meraih medali emas untuk pertama kalinya. Prestasi ini menjadi momen bersejarah bagi perkembangan catur di Indonesia.',
     },
     {
-      date: '2000 M',
-      desc: 'Silat diakui sebagai bagian dari budaya Suku Melayu dalam pengertian yang luas, mencakup penduduk pulau Sumatra, Semenanjung Malaka, Jawa, Bali, Kalimantan, Sulawesi, dan pulau-pulau lainnya yang menggunakan bahasa Melayu. Pencak silat menjadi tak terpisahkan dari upacara adat dan kesenian masyarakat di berbagai daerah.',
+      date: 'Perkembangan Prestasi Catur Indonesia',
+      desc: 'Indonesia terus menunjukkan perkembangan prestasi catur di tingkat internasional, baik dalam Olimpiade Catur maupun kompetisi-kompetisi lainnya. Beberapa caturawan Indonesia mencatat prestasi gemilang di tingkat nasional dan internasional. Keikutsertaan mereka membawa nama Indonesia sebagai salah satu negara dengan pemain catur yang mampu bersaing di tingkat dunia.',
     },
     {
-      date: 'Saat ini',
-      desc: 'Perguruan-perguruan silat tumbuh di Amerika Serikat dan Eropa. Silat diakui sebagai cabang olahraga dalam pertandingan internasional, khususnya dipertandingkan dalam ajang SEA Games.',
+      date: '',
+      desc: '',
     },
   ];
 
   return (
     <div className='sport-container catur-container'>
       <img src={bgCatur} alt='bg-silat' className='bg-catur' />
+      <BackgroundAwan />
       <div className='sport-section'>
         <div className='sport-content-1'>
           {dataExperience.map((experience, id, arr) => (
